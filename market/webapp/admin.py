6d926e5d11227eb897bd9c442be8ec1046b57f16
bermet <zabylparol123456>
2019-04-06 15:11:05 +0600

    Добавлены модели

--- market/webapp/admin.py
+++ market/webapp/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from webapp.models import Category, Item, ItemImage, Order
 
-# Register your models here.
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'arrive_date']
+    ordering = ['pk']
+    search_fields = ['name', 'id']
+
+admin.site.register(Item, ItemAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name']
+    ordering = ['name']
+    search_fields = ['name', 'id']
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class ItemImageAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'item']
+    ordering = ['pk']
+    search_fields = ['item', 'id']
+
+admin.site.register(ItemImage, ItemImageAdmin)
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'user', 'phone', 'date']
+    ordering = ['-date']
+    search_fields = ['user', 'id']
+
+admin.site.register(Order, OrderAdmin)
--- market/webapp/models.py
+++ market/webapp/models.py
@@ -1,3 +1,52 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class SoftDeleteManager(models.Manager):
+    def active(self):
+        return self.filter(is_deleted=False)
+
+    def deleted(self):
+        return self.filter(is_deleted=True)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=2000, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
+class Item(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField(max_length=2000, null=True, blank=True)
+    arrive_date = models.DateField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    categories = models.ManyToManyField('Category', related_name='items', blank=True)
+    is_deleted = models.BooleanField(default=False)
+
+    objects = SoftDeleteManager()
+
+    def __str__(self):
+        return self.name
+
+class ItemImage(models.Model):
+    item = models.ForeignKey('Item', related_name='images', on_delete=models.PROTECT)
+    image = models.ImageField(upload_to='images', default='/images/default.img')
+
+class Order(models.Model):
+    user = models.ForeignKey(User, related_name='orders', on_delete=models.PROTECT)
+    items = models.ForeignKey('Item', related_name='order', on_delete=models.PROTECT)
+    phone = models.CharField(max_length=255)
+    address = models.CharField(max_length=500, null=True, blank=True)
+    comment = models.TextField(max_length=2000, null=True, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    is_deleted = models.BooleanField(default=False)
+
+    objects = SoftDeleteManager()
+
+    def __str__(self):
+        return 'Order for: %s, Items: %s' % (self.user, self.items)
