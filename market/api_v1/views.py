45f46561110172cc1a418f20e477986b50945c2c
bermet <zabylparol123456>
2019-04-06 15:12:15 +0600

    Добавлены вьюсеты

--- market/api_v1/views.py
+++ market/api_v1/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from webapp.models import Item, ItemImage, Category, Order
+from rest_framework import viewsets
+from api_v1.serializers import ItemSerializer, CategorySerializer, OrderSerializer, ItemImageSerializer
 
-# Create your views here.
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all().order_by('arrive_date')
+    serializer_class = ItemSerializer
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all().order_by('name')
+    serializer_class = CategorySerializer
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all().order_by('id')
+    serializer_class = OrderSerializer
+
+class ItemImageViewSet(viewsets.ModelViewSet):
+    queryset = ItemImage.objects.all().order_by('id')
+    serializer_class = ItemImageSerializer
